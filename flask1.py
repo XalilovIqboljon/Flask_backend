9e4e36b9ee2389d7567f30b3b39a275e03c3dac2
Xalilov <eljjey@gmail.com>
2021-02-24 15:55:37 +0500

    def home

--- flask1.py
+++ flask1.py
@@ -1,10 +1,23 @@
 from flask import Flask
 
 app = Flask(__name__)
+h1='''
+    <h1>Assalom aleykum<h2>
+    <h2>Assalom aleykum<h2>
+    <h3>Assalom aleykum<h3>
+    <h4>Assalom aleykum<h4>
+'''
 
 @app.route('/')
 def hello():
     return 'Hello World'
 
+@app.route('/index')
+def index():
+    return "<h1>Assalom <p><h2>aleykum<h2><h1>"
+@app.route('/home')
+def home():
+    return h1
 
-app.run()
+
+app.run(debug=True)
