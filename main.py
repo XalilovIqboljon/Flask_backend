440273b81582bf417e4f2adb0126a29bda624689
Xalilov <eljjey@gmail.com>
2021-02-24 12:53:52 +0500

    list to lista

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('hi')
+# [i for i, e in enumerate([1, 2, 1]) if e == 1]
+list1=[1,2,1,2,3,4,5,1,2,3,1]
+list2=[]
+for i,a in enumerate(list1):
+    if a==1:
+        list2.append(i)
+
+print(list2)
