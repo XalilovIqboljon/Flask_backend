520dc8a157d4cd56a697a954be2cb97e4b342eb3
Xalilov <eljjey@gmail.com>
2021-02-24 16:10:11 +0500

    jadval

--- flask1.py
+++ flask1.py
@@ -7,14 +7,32 @@ h1='''
     <h3>Assalom aleykum<h3>
     <h4>Assalom aleykum<h4>
 '''
+commen='''
+    <table border="1">
+        <tr>
+            <td>Ism</td>
+            <td>Familiya</td>
+            <td>Otasini ismi</td>
+        </tr>
+        <tr>
+            <td>Iqboljon</td>
+            <td>Xalilov</td>
+            <td>Qodirjon o'g'li</td>
+        </tr>
+'''
 
 @app.route('/')
 def hello():
     return 'Hello World'
 
+@app.route('/comment')
+def comm():
+    return commen
+
 @app.route('/index')
 def index():
     return "<h1>Assalom <p><h2>aleykum<h2><h1>"
+
 @app.route('/home')
 def home():
     return h1
